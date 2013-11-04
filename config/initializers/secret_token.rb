2557fcf7809563f2abd297233e4be0fc863bdda5
yulin <luckyyulin@sina.cn>
2013-11-04 20:13:30 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lin::Application.config.secret_token = '7a84c5e2f299fa5257c772471ab157058cb1757c10a2f8130502f4766aeb59ea74607883b889368b6f3b496ce1e1a230bddc66d96ef9ac7a9ccd4e03b1d7e466'
